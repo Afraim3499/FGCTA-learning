463d58914e0e7bf3e05780985cc75d23aeab0382
Rizwanul Islam <afraim.afraim99@gmail.com>
2026-07-15 09:28:21 +0600

    Upgrade mixed asset intelligence batch seven

--- app/src/lib/asset-intelligence-data.ts
+++ app/src/lib/asset-intelligence-data.ts
@@ -3,6 +3,7 @@ import { assetDeepBatch3Enhancements } from "./asset-intelligence-deep-batch-3";
 import { assetDeepBatch4Enhancements } from "./asset-intelligence-deep-batch-4";
 import { assetDeepBatch5Enhancements } from "./asset-intelligence-deep-batch-5";
 import { assetDeepBatch6Enhancements } from "./asset-intelligence-deep-batch-6";
+import { assetDeepBatch7Enhancements } from "./asset-intelligence-deep-batch-7";
 
 export type AssetClassKey = "crypto" | "forex" | "gold";
 export type AssetCoverageStatus = "ready" | "planned" | "archived";
@@ -7023,6 +7024,7 @@ const baseAssetProfiles: AssetProfile[] = [
 
 export const assetProfiles: AssetProfile[] = baseAssetProfiles.map((asset) => {
   const enhancement =
+    assetDeepBatch7Enhancements[asset.slug] ??
     assetDeepBatch6Enhancements[asset.slug] ??
     assetDeepBatch5Enhancements[asset.slug] ??
     assetDeepBatch4Enhancements[asset.slug] ??
